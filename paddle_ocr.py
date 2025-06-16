9a1b9ffcc757d87fb628d43f897c9e8f4f6865ec
vatsj <jacobstavrianos@gmail.com>
2025-06-15 23:58:51 -0500

    result parsing bugfix

--- paddle_ocr.py
+++ paddle_ocr.py
@@ -1,14 +1,19 @@
 # notebook: https://colab.research.google.com/drive/1-obW-2th3xQlAZZuHhcz-B1XTnOLkcAq#scrollTo=Ldo7HuOA5O4M
 
 from paddleocr import PaddleOCR
+import numpy as np
 
 def result_to_text(result):
-    return '\n'.join([line[1][0] for line in result])
+    return '\n'.join([line[1][0] for line in result[0]])
 
-ocr = PaddleOCR(use_angle_cls=True)
-def ocr(docs: list):
+paddle_ocr_engine = PaddleOCR(use_angle_cls=True)
+
+def ocr(pages: list):
     result_texts = []
-    for i, doc in enumerate(docs):
+    for i, page in enumerate(pages):
         print('processing page', i)
-        result_texts.append(result_to_text(ocr.ocr(doc, cls=True)))
+        # Convert PIL image to numpy array for PaddleOCR
+        page_array = np.array(page)
+        result = paddle_ocr_engine.ocr(page_array, cls=True)
+        result_texts.append(result_to_text(result))
     return result_texts
